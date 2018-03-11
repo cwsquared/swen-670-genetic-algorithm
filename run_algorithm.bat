e38450817e88a33c1b132e444425344ab04d23b3
Amanda <alee143@student.umuc.edu>
2018-03-11 11:20:21 -0400

    Clean up bat script output
    Made the bat script output to the command line more readable.

--- run_algorithm.bat
+++ run_algorithm.bat
@@ -1,4 +1,4 @@
-
+ECHO OFF
 REM This script will run the genetic algorithm program with certain parameters.
 REM 	[0] = fitness function
 REM		[1] = POP_SIZE
@@ -9,26 +9,37 @@ REM		[5] = METHYLATION_COUNT
 
 SET filename=genetic_algorithm_output.csv
 
+ECHO.
+ECHO Output file is located at: 
+ECHO %CD%\%filename%
+ECHO. 
+PAUSE
+
 :: Clears out genetic_algorithm_output.csv
 break > %filename%
 
-REM Fitness Function #1
+ECHO.
+ECHO Fitness Function #1
 java -classpath ./build/GeneticAlgorithm.jar geneticalgorithm/ResearchGA 1 2 2 8 0.2 2 >> %filename%
 java -classpath ./build/GeneticAlgorithm.jar geneticalgorithm/ResearchGA 1 8 2 16 0.5 2 >> %filename%
 
-ECHO.>>%filename%
+::ECHO.>>%filename%
 
-REM Fitness Function #2
+::ECHO.
+::ECHO Fitness Function #2
 ::java -classpath ./build/GeneticAlgorithm.jar geneticalgorithm/ResearchGA 2 2 1 5 0.2 2 >> %filename%
 
-ECHO.>>%filename%
+::ECHO.>>%filename%
 
-REM Fitness Function #3
+::ECHO.
+::ECHO Fitness Function #3
 
-ECHO.>>%filename%
+::ECHO.>>%filename%
 
-REM Fitness Function #4
+::ECHO.
+::ECHO Fitness Function #4
 
-ECHO.>>%filename%
+::ECHO.>>%filename%
 
-REM Fitness Function #5
+::ECHO.
+::ECHO Fitness Function #5
