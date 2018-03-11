ae09535867645a6237eb49689449fbc6971fb25c
Amanda <alee143@student.umuc.edu>
2018-03-11 10:58:47 -0400

    Fix endless loop in printGeneration
    Fixed issue where there was an endless loop in printGeneration.
    Cleaned up batch script.

--- run_algorithm.bat
+++ run_algorithm.bat
@@ -1,7 +1,15 @@
+
 REM This script will run the genetic algorithm program with certain parameters.
+REM 	[0] = fitness function
+REM		[1] = POP_SIZE
+REM		[2] = NUM_OF_GENERATIONS
+REM		[3] = NUM_GENES_PER_INDIVIDUAL
+REM		[4] = MUTATION_RATE 
+REM		[5] = METHYLATION_COUNT
 
 SET filename=genetic_algorithm_output.csv
 
+:: Clears out genetic_algorithm_output.csv
 break > %filename%
 
 REM Fitness Function #1
@@ -11,7 +19,7 @@ java -classpath ./build/GeneticAlgorithm.jar geneticalgorithm/ResearchGA 1 8 2 1
 ECHO.>>%filename%
 
 REM Fitness Function #2
-java -classpath ./build/GeneticAlgorithm.jar geneticalgorithm/ResearchGA 2 2 1 5 0.2 2 >> %filename%
+::java -classpath ./build/GeneticAlgorithm.jar geneticalgorithm/ResearchGA 2 2 1 5 0.2 2 >> %filename%
 
 ECHO.>>%filename%
 
