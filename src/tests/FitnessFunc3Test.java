c0c8a2d65aacbe6ad9009b3b67e1ff2b653c8f46
Kingsley Usen <kusen@student.umuc.edu>
2018-03-24 17:35:27 -0400

    Added unit tests for fitness function 3

--- src/tests/FitnessFunc3Test.java
+++ src/tests/FitnessFunc3Test.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import geneticalgorithm.FitnessFunc2;
 import geneticalgorithm.FitnessFunc3;
 
 public class FitnessFunc3Test {
@@ -25,5 +26,30 @@ public class FitnessFunc3Test {
 		assertEquals(f3.getLowerBound(), -1);
 		assertEquals(f3.getUpperBound(), 6);
 	}
+	
+	@Test
+	// Test that the getFitness method returns correct value for x = 0.
+	public void testGetFitnessFunc3Zero() {
+		FitnessFunc3 f3 = new FitnessFunc3(1, 0, 1);
+		assertEquals(f3.getFitness("0000000000000000"), 0.0, 1E-10);
+	}
+	
+	@Test
+	// Test that the getFitness method returns correct value for x = 1.
+	public void testGetFitnessFunc3One() {
+		FitnessFunc3 f3 = new FitnessFunc3(1, 0, 1);
+		assertEquals(f3.getFitness("1111111111111111"), 0.0, 1E-10);
+	}
+	
+	@Test
+	// Test that the getFitness method returns correct value for x = 0.
+	public void testGetFitnessFunc3OneFifteenth() {
+		FitnessFunc3 f3 = new FitnessFunc3(1, 0, 1);
+		Double ret = f3.getFitness("0000110111001110");
+//		System.out.println("===> "+ret);  ///--0.04123937851565791
+		assertEquals(ret , 1.902692726297746E-4, 1E-10);
+	}
+	
+	
 	
 }
