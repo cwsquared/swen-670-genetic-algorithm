94bd9966abc210a8bc783f1a64f142c26790b7cc
Charles Enwesi <cenwesi@gmail.com>
2018-03-28 23:19:02 -0400

    Fix Fitnessfunction4Test
    Thank you Chris for catching my error with using 5 instead of 10 and
    fixing it.

--- src/tests/FitnessFunc4Test.java
+++ src/tests/FitnessFunc4Test.java
@@ -54,7 +54,7 @@ public class FitnessFunc4Test {
 		String[][] ind = new String[1][2];
 		ind[0][0] = "1100110011001100";
 		ind[0][1] = "0000000000000000";
-		assertEquals(f4.getFitness(ind) , 0.44319042360045985, 1E-10);
+		assertEquals(f4.getFitness(ind) , 2.4121809515799384E-94, 1E-10);
 	}
 	
 	
